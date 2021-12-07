ea888d200183869bb0c8686f64282c4e1cb8d81d
SinJungho <zmtls0516@naver.com>
2021-12-07 21:39:19 +0900

    timer start btn adjustment & finish

--- js/timer.js
+++ js/timer.js
@@ -1,34 +1,50 @@
-const timer = document.querySelector('.timer__text');
-let start = document.querySelector('.timer__btn__start').addEventListener('click', startTimer);
-let stop = document.querySelector('.timer__btn__stop').addEventListener('click', stopTimer);
+let timerId;
+let time = 0;
+let hour, min, sec;
+const startBtn = document.querySelector(".timer__btn__start");
+let stopBtn = document.querySelector(".timer__btn__stop");
+const stopwatch = document.querySelector(".timer__text");
 
-let TIME = 0;
-let cron;
+function printTime() {
+  time++;
+  stopwatch.innerText = getTimeFormatString();
+}
 
+//시계 시작 - 재귀호출로 반복실행
 function startTimer() {
-	printTimer();
-	// stopTimer();
-	corn = setInterval(printTimer, 1000);
-	timer.classList.add('hide');
-	console.log('hello');
+  printTime();
+  stopTimer();
+  startBtn.classList.add("hide");
+  timerId = setTimeout(startTimer, 1000);
+  console.log('hello');
 }
 
+//시계 중지
 function stopTimer() {
-	clearInterval(corn);
-	timer.classList.remove('hide');
-	console.log('hello');
+  if (timerId != null) {
+    clearTimeout(timerId);
+    startBtn.classList.remove("hide");
+  }
 }
 
-
-function printTimer() {
-	const hours = Math.floor(TIME / 3600);
-	const checkMinutes = Math.floor(TIME / 60);
-	const second = TIME % 60;
-	const minutes = checkMinutes % 60;
-
-	timer.innerText = `${hours < 10 ? `0${hours} ` : hours} : ${minutes < 10 ? `0${minutes}` : minutes} : ${second < 10 ? `0${second}` : second}`; 
-	TIME++;
+// 시계 초기화
+function resetTimer() {
+  stopTimer();
+  stopwatch.innerText = "00:00:00";
+  time = 0;
 }
-startTimer();
-// start.addEventListener('click', startTimer);
-// stop.addEventListener('click',  stopTimer);
+
+// 시간(int)을 시, 분, 초 문자열로 변환
+function getTimeFormatString() {
+  hour = parseInt(String(time / (60 * 60)));
+  min = parseInt(String((time - hour * 60 * 60) / 60));
+  sec = time % 60;
+
+  return (
+    String(hour).padStart(2, "0") +
+    ":" +
+    String(min).padStart(2, "0") +
+    ":" +
+    String(sec).padStart(2, "0")
+  );
+}

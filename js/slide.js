94fb043292efe156a4d02c683219c2c3e64f6237
SinJungho <zmtls0516@naver.com>
2021-12-13 22:46:02 +0900

    slideNext addEventLisner add

--- js/slide.js
+++ js/slide.js
@@ -1,5 +1,14 @@
 const slideBox = document.querySelector('.slide__box');
-const slideItem = document.querySelector('.slide__item');
+const slideItem = document.querySelectorAll('.slide__item');
 const slideNext = document.querySelector('.slide__btn__next');
 const slidePrev = document.querySelector('.slide__btn__prev');
 const slideDots = document.querySelector('.slide__dots');
+const slideLen = slideItem.length;
+const slideWidth = 960;
+const slideSpeed = 400;
+
+let curIndex = 0;
+
+slideNext.addEventListener('click', function () {
+  
+});

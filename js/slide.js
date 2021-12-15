57729433076ce988fc9210bf0e9400207b456972
SinJungho <zmtls0516@naver.com>
2021-12-15 23:20:32 +0900

    img slide prototype adjustment

--- js/slide.js
+++ js/slide.js
@@ -4,11 +4,21 @@ const slideNext = document.querySelector('.slide__btn__next');
 const slidePrev = document.querySelector('.slide__btn__prev');
 const slideDots = document.querySelector('.slide__dots');
 const slideLen = slideItem.length;
-const slideWidth = 960;
+const slideWidth = 460;
 const slideSpeed = 400;
+
+slideBox.style.width = slideWidth * (slideLen) + 'px';
 
 let curIndex = 0;
 
 slideNext.addEventListener('click', function () {
-  
+  if (curIndex < slideLen - 1) {
+    slideBox.style.transition = slideSpeed + 'ms';
+    slideBox.style.transform = 'translate3d(-'+(slideWidth * (curIndex + 1))+'px, 0px, 0px)';  
+  } else{
+    slideBox.style.transform = 'translate3d(0px, 0px, 0px)';
+    curIndex = -1;
+  }
+  curSlide = slideItem[++curIndex];
 });
+
